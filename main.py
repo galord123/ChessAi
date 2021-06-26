735689b5f7eb9bfcc7be81dceb7937a4564c9caa
gal holtzman <galbuba@gmail.com>
2021-06-26 17:17:08 +0300

    basic minimax algo added

--- main.py
+++ main.py
@@ -1,48 +1,249 @@
 import chess
-import random
+import chess.engine
+import chess.pgn
+import collections
 from os import system
 
 
-board = chess.Board()
+# Evaluating the board
+pawntable = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, -20, -20, 10, 10, 5,
+    5, -5, -10, 0, 0, -10, -5, 5,
+    0, 0, 0, 20, 20, 0, 0, 0,
+    5, 5, 10, 25, 25, 10, 5, 5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+knightstable = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50]
+
+bishopstable = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20]
+
+rookstable = [
+    0, 0, 0, 5, 5, 0, 0, 0,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+queenstable = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20]
+
+kingstable = [
+    20, 30, 10, 0, 0, 10, 30, 20,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30]
 
 
-playing = True
-turn = False
-
-
-while playing:
-    good_move = True
-    turn = not turn
-    if turn:
-        print("white to move")
-    else:
-        print("black to move")
-
-    if turn:
-        while good_move:
-            move = input()
-            try:
-                board.push_san(move)
-            except ValueError:
-                good_move = False
-
-            good_move = not good_move
-    else:
-        board.push(random.choice(list(board.legal_moves)))
-
-
-
-    _ = system("cls")
-    print(board)
-
+def evaluate_board(board):
     if board.is_checkmate():
-        if turn:
-            print("white won!")
+        if board.turn:
+            return -9999
         else:
-            print("black won!")
-
-        playing = False
-
+            return 9999
     if board.is_stalemate():
-        print("stale mate")
-        playing = False
+        return 0
+    if board.is_insufficient_material():
+        return 0
+
+    wp = len(board.pieces(chess.PAWN, chess.WHITE))
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+
+    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.PAWN, chess.BLACK)])
+    knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+    knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+    bishopsq = sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+    bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.BISHOP, chess.BLACK)])
+    rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+    rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.ROOK, chess.BLACK)])
+    queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+    queensq = queensq + sum([-queenstable[chess.square_mirror(i)]
+                             for i in board.pieces(chess.QUEEN, chess.BLACK)])
+    kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)])
+    kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.KING, chess.BLACK)])
+
+    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
+    if board.turn:
+        return eval
+    else:
+        return -eval
+
+
+def minimaxRoot(depth, game: chess.Board, isMaximisingPlayer):
+
+    newGameMoves = game.generate_legal_moves()
+    bestMove = -9999
+    bestMoveFound = chess.Move.null()
+
+    for move in newGameMoves:
+        
+        game.push(move)
+        value = minimax(depth - 1, game, -10000, 10000, not isMaximisingPlayer)
+        game.pop()
+        #print("cur move", value, move)
+        if value >= bestMove:
+            bestMove = value
+            bestMoveFound = move
+            #print("best val found", bestMove, bestMoveFound)
+    
+    return bestMoveFound
+
+
+def minimax (depth, game: chess.Board, alpha, beta, isMaximisingPlayer):
+    
+    if depth == 0 or game.is_game_over():
+        return evaluate_board(game)
+
+    moves = game.generate_legal_moves()
+
+    if (isMaximisingPlayer):
+        bestMove = -9999
+        for move in moves:
+            game.push(move)
+            bestMove = max(bestMove, minimax(depth - 1, game, alpha, beta, not isMaximisingPlayer))
+            game.pop()
+            alpha = max(alpha, bestMove)
+            if (beta <= alpha):
+                return bestMove
+            
+        return bestMove
+
+    else:
+        bestMove = 9999
+        for move in moves:
+            game.push(move)
+            bestMove = min(bestMove, minimax(depth - 1, game, alpha, beta, not isMaximisingPlayer))
+            game.pop()
+            beta = min(beta, bestMove)
+            if (beta <= alpha):
+                return bestMove
+            
+        
+        return bestMove
+    
+
+
+def board_to_game(board):
+    game = chess.pgn.Game()
+
+    # Undo all moves.
+    switchyard = collections.deque()
+    while board.move_stack:
+        switchyard.append(board.pop())
+
+    game.setup(board)
+    node = game
+
+    # Replay all moves.
+    while switchyard:
+        move = switchyard.pop()
+        node = node.add_variation(move)
+        board.push(move)
+
+    game.headers["Result"] = board.result()
+    return game
+
+
+
+def main():
+    board = chess.Board()
+
+
+    playing = True
+    turn = False
+
+
+    while playing:
+        good_move = True
+        turn = not turn
+        if turn:
+            print("white to move")
+            print(evaluate_board(board))
+        else:
+            print("black to move")
+
+        print(board_to_game(board)) 
+
+        if not turn:
+            engine = chess.engine.SimpleEngine.popen_uci("C:\\files\\chessEn.exe")
+            engine.play(board, chess.engine.Limit(time=2.0))
+
+            if False:
+                while good_move:
+                    move = input()
+                    try:
+                        board.push_san(move)
+                    except ValueError:
+                        good_move = False
+
+                    good_move = not good_move
+        else:
+            # Ai Logic
+            best_move = minimaxRoot(4, board, True)
+            board.push(best_move)
+        _ = system("cls")
+        print(board.mirror())
+
+        if board.is_checkmate():
+            if turn:
+                print("white won!")
+            else:
+                print("black won!")
+
+            playing = False
+
+        if board.is_stalemate():
+            print("stale mate")
+            playing = False
+
+
+if __name__ == "__main__":
+    main()
